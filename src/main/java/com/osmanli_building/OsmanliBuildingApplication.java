701328b592a7a2ce32b3ee590dd19c1b61b8c74d
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-05 09:52:50 +0300

    database integration

---
+++ src/main/java/com/osmanli_building/OsmanliBuildingApplication.java
@@ -0,0 +1,13 @@
+package com.osmanli_building;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmanliBuildingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmanliBuildingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osmanli_building/OsmanliBuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmanli_building;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmanliBuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
