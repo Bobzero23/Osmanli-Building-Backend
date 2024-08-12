0a016b0f8e9e40900dc6b755ad54c26f053c95de
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-12 14:50:25 +0300

    feature: creating product repository layer

---
+++ src/main/java/com/osmanli_building/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.osmanli_building.repository;
+
+import com.osmanli_building.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
